cec2ac6e7f431dc5559d35291a8923dc8c52694b
Rômulo Fernandes <romulo.fernandes@gmail.com>
2014-09-04 17:10:41 -0300

    RC1: IE9+ full support, IE8 support with flaws

---
+++ config.rb
@@ -0,0 +1,11 @@
+preferred_syntax = :scss
+relative_assets = true
+line_comments = false
+sourcemap = true
+output_style = :compressed
+
+http_path = '/'
+css_dir = 'css'
+sass_dir = 'sass'
+images_dir = 'img'
+fonts_dir = 'fonts'
